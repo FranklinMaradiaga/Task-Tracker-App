9d2c82bf22ced3d03d0682cae73380754bee53e2
Franklin Maradiaga <franklinmaradiaga@franklislaptop2.newcastle.edu.au>
2024-05-25 13:23:40 +1000

    Initialize app state variables and cached element references

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+// Section 1: TODOs
+// TODO: Register submissions from the user on the form.
+// TODO: Determine the value of the data submitted and add it to a JavaScript array calle
+// TODO: Call the render function to update the table with the new tasks.
+
+let tasks = [];
+
+// Section 3: Cached Element References
+const taskForm = document.getElementById("taskForm");
+const taskTable = document.getElementById("taskTable");
+
+
+console.log( "Form: ", taskForm )
+console.log( "Table: ", taskTable )
